855f7e9bd15f230842e9155078fd26221de6e9fa
Darwin Vilcachagua Ayala <Darwin97.VA@gmail.com>
2020-06-20 13:22:38 -0500

    presentado 2

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const input = 'R3, L2, L2, R4, L1, R2, R3, R4, L2, R4, L2, L5, L1, R5, R2, R2, L1, R4, R1, L5, L3, R4, R3, R1, L1, L5, L4, L2, R5, L3, L4, R3, R1, L3, R1, L3, R3, L4, R2, R5, L190, R2, L3, R47, R4, L3, R78, L1, R3, R190, R4, L3, R4, R2, R5, R3, R4, R3, L1, L4, R3, L4, R1, L4, L5, R3, L3, L4, R1, R2, L4, L3, R3, R3, L2, L5, R1, L4, L1, R5, L5, R1, R5, L4, R2, L2, R1, L5, L4, R4, R4, R3, R2, R3, L1, R4, R5, L2, L5, L4, L1, R4, L4, R4, L4, R1, R5, L1, R1, L5, R5, R1, R1, L3, L1, R4, L1, L4, L4, L3, R1, R4, R1, R1, R2, L5, L2, R4, L1, R3, L5, L2, R5, L4, R5, L5, R3, R4, L3, L3, L2, R2, L5, L5, R3, R4, R3, R4, R3, R1'.split(', ')
-// 3R = 1L
-// 3L = 1R
+
 const santa = {
     go: 'y',
-    // goY: +1,
-    // goX: +1,
     x: 0,
     y: 0
 }
+
 const going = (change, num) => {
     if(change === 'R') {
         switch(santa.go) {
@@ -60,10 +58,7 @@ input.forEach(goes=>{
     const number = num(goes)
     going(change, number)
 })
-// const Rs = input.filter(go=>str(go)==='R')
-// const Ls = input.filter(go=>str(go)==='L')
-// const R3 = input.filter(go=>go==='R3')
 
-console.log(santa
+console.log(
+    santa
 )
-
